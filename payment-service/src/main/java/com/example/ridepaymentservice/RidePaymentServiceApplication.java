79a6d30611d0806a4c977f62a58afdd0fd53fc4e
Siddhi Mehendale <49885480+SiddhiM22@users.noreply.github.com>
2025-05-10 13:38:00 +0530

    Add files via upload

---
+++ payment-service/src/main/java/com/example/ridepaymentservice/RidePaymentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.ridepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidePaymentServiceApplication.class, args);
+    }
+}
